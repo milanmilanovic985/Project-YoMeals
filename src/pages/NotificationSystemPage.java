76cdf1a264ff8d2ece2fc17a345202ad539df41a
Milan Milanovic <milan.milanovic985@gmail.com>
2022-02-25 13:55:27 +0100

    Notification System Page

---
+++ src/pages/NotificationSystemPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NotificationSystemPage extends BasicPage{
+
+	public NotificationSystemPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		super(driver, wait, js);
+		
+	}
+	public WebElement getMessageElement () {
+		return driver.findElement(By.xpath("//*[contains(@class, 'alert--success')"));
+	}
+	public String notificationMessage () {
+		return getMessageElement().getText();
+	}
+	public void waitUntilNotificationDisappear () {
+		wait.until(ExpectedConditions.attributeContains(By.xpath("//*[contains(@class, 'system_message')]"), "style", "display: none;"));
+	}
+
+}
